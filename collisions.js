a08532122fd659c716ba199486a978fa3e105f8e
Evan Morschhauser <evanmorsch@gmail.com>
2021-03-17 11:13:42 -0400

    Update collisions.js

--- collisions.js
+++ collisions.js
@@ -1,38 +1,52 @@
+//will change to actually take the bodies in instead of just their indexes
+//shoudl probably seperate the distance checking from this function
+//might replace with a completely new function for the speed improvements that it would bring, this is accurate and cool, but so slow
+//this was just a copy and paste function initially, i tweaked it over the years but i still dont completely understamd it
 function Collision(i,j)	{
-	set=[bodies[i].x,bodies[i].y];
-	set2=[bodies[j].x,bodies[j].y];
-	delta = subtract(set,set2);
-	r=(bodies[i].radius)+(bodies[j].radius);
-	dist2=dot(delta,delta);
-	if (dist2 > r*r) return;
-	d = getLength(delta);
-	if (d != 0) {
-		mtd = multiply(delta,(((bodies[i].radius+bodies[j].radius)-d)/d));
+	//a slightly faster version of finding distance
+	//i probably thought it was smart to do this when i was younger
+	var delta = subtract(bodies[i].pos,bodies[j].pos);//coordinate offset of bodies
+	var r = (bodies[i].radius)+(bodies[j].radius);//impact distance
+	var dist2 = dot(delta,delta);//dot product of delta
+	if (dist2 > r*r) return;//if the bodies havent hit yet, bail out
+	//now that were sure the bodies shoiudl be colliding, we resolve the collision
+	var d = mag(delta);//actual distance (couldve just done this from the start...)
+	if (d != 0) {//check if the bodies are completely overlapped
+		//"minimum translation distance" vecotor
+		var mtd = multiply(delta,(((bodies[i].radius+bodies[j].radius)-d)/d));
 	} else {
-		d = bodies[i].radius+bodies[j].radius-1;
-		delta = [bodies[i].radius+bodies[j].radius,0];
-		mtd = multiply(delta,(((bodies[i].radius+bodies[j].radius)-d)/d));
+		//just basically tells the program to shift the bodies apart on the x axis
+		//could just set mtd to 0,0 as i think it always leaves this block as 0,0 anyway
+		d = bodies[i].radius+bodies[j].radius;
+		delta = new _vector(bodies[i].radius+bodies[j].radius,0);
+		var mtd = multiply(delta,(((bodies[i].radius+bodies[j].radius)-d)/d));
 	}
-	im1 = 1/bodies[i].m;
-	im2 = 1/bodies[j].m;
-	temp = multiply(mtd,im1/(im1+im2));
-	temp2 = multiply(mtd,im2/(im1+im2));
-	bodies[i].x+=temp[0];
-	bodies[i].y+=temp[1];
-	bodies[j].x-=temp2[0];
-	bodies[j].y-=temp2[1];
-	set=[bodies[i].vx,bodies[i].vy];
-	set2=[bodies[j].vx,bodies[j].vy];
-	v=subtract(set,set2);
-	vn=dot(v,normalize(mtd));
-	if (vn > 0)	return;
-	ii = (-(1 + restitution) * vn) / (im1 + im2);
-	impulse=multiply(mtd,ii);
-	temp=multiply(impulse,im1);
-	temp2=multiply(impulse,im2);
-	bodies[i].vx+=temp[0];
-	bodies[i].vy+=temp[1];
-	bodies[j].vx-=temp2[0];
-	bodies[j].vy-=temp2[1];
-	};	
+	//impluses of the bodies
+	var im1 = 1/bodies[i].m;
+	var im2 = 1/bodies[j].m;
+	//i think this is the positional shift to be applied to the body due to it being moved apart
+	var temp = multiply(mtd,im1/(im1+im2));
+	var temp2 = multiply(mtd,im2/(im1+im2));
+	//shift the bodies by the above amount
+	bodies[i].x+=temp.x;
+	bodies[i].y+=temp.y;
+	bodies[j].x-=temp2.x;//think about flipping the sign on temp2 so this can be an adding function
+	bodies[j].y-=temp2.y;
+	//total velocity invloved in the collision
+	var v = subtract((bodies[i].vel,bodies[j].vel);
+	//the normalized velocity i think
+	var vn = dot(v,normalize(mtd));
+	//i guess thjat cant be less than 0?..
+	if (vn > 0) return;
+	//i really dont get this one... it must be applying the restitution of the bodies to something to 
+	//giving up on decyphering this for now, ill come back to it
+	var ii = (-(1 + restitution) * vn) / (im1 + im2);
+	var impulse = multiply(mtd,ii);
+	temp = multiply(impulse,im1);
+	temp2 = multiply(impulse,im2);
+	bodies[i].vx+=temp.x;
+	bodies[i].vy+=temp.y;
+	bodies[j].vx-=temp2.x;
+	bodies[j].vy-=temp2.y;
+};	
     
