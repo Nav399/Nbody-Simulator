8e46953ee7dfa7dea044dfb92bdb9f87ff0e0729
Evan Morschhauser <evanmorsch@gmail.com>
2021-03-17 11:24:12 -0400

    Update nbody.js

--- nbody.js
+++ nbody.js
@@ -3,21 +3,21 @@ function nbody(collide) {
 		for (var i = 0; i < bodies.length; i++) {
 			if (bodies[i].movable===false) {continue};
 			//Set initial acceleration to 0
-			bodies[i].ax = 0;
-			bodies[i].ay = 0;	
+			bodies[i].acc.x = 0;
+			bodies[i].acc.y = 0;	
 			for (var j = 0; j < bodies.length; j++) {
 				//Check that not testing the same particle twice
 				if (i === j) {continue;};
 				var minDist = ((bodies[i].radius+bodies[j].radius))
 				//Calculate the x-y-Total distances between bodies
-				var dist = FindDist(bodies[i].x,bodies[i].y,bodies[j].x,bodies[j].y);
+				var dist = FindDist(bodies[i].pos.x,bodies[i].pos.y,bodies[j].pos.x,bodies[j].pos.y);
 				//make r either the distance or e, whichever is larger
 				var r = Math.max(dist.Total,minDist);
 				//Calculate force from the distance and masses
 				var f = FindForce(bodies[i].m,bodies[j].m,r);
 				//Apply force to the acceleration (subtract for attractive force)
-				bodies[i].ax -= (f * (dist.dx/r))/bodies[i].m;
-				bodies[i].ay -= (f * (dist.dy/r))/bodies[i].m;
+				bodies[i].acc.x -= (f * (dist.dx/r))/bodies[i].m;
+				bodies[i].acc.y -= (f * (dist.dy/r))/bodies[i].m;
 			
 				if ((dist.Total <= ((bodies[i].radius+bodies[j].radius))) && collide===true) {
 					//Check if bodies are colliding
@@ -26,11 +26,12 @@ function nbody(collide) {
 				};
 			};
 		};
+		//apply movements
 		for (var i = 0; i < bodies.length; i++) {
-			bodies[i].vx += (bodies[i].ax * dt);
-			bodies[i].vy += (bodies[i].ay * dt);
-			bodies[i].x += bodies[i].vx * dt;
-			bodies[i].y += bodies[i].vy * dt;
+			bodies[i].vel.x += (bodies[i].acc.x * dt);
+			bodies[i].vel.y += (bodies[i].acc.y * dt);
+			bodies[i].pos.x += bodies[i].vel.x * dt;
+			bodies[i].pos.y += bodies[i].vel.y * dt;
 		};
 		fps=getFramerate();
 		draw(true);
